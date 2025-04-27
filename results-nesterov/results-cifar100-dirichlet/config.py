6816f78fd8bb6a90672c894256c7e10dd92ac0ca
aamirmd <aamirmd105@gmail.com>
2025-04-27 12:18:05 -0400

    Add cifar100 results

---
+++ results-nesterov/results-cifar100-dirichlet/config.py
@@ -0,0 +1,11 @@
+class Config:
+    NUM_CLIENTS = 10
+    NUM_ROUNDS = 50
+    BATCH_SIZE = 32
+    MODEL_NAME = "net"
+    DATASET = 'cifar100' # Literal['cifar10', 'cifar100', 'mnist']
+    PARTITIONER_TYPE = 'dirichlet' # Literal['uniform', 'dirichlet']
+    DIRILECT_ALPHA = 0.5 # parameter when using dirichlet (means how close to iid it is)
+    OPTIMIZER='nesterov'
+    MOMENTUM=0.9
+    LEARNING_RATE=0.06
