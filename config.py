5c4cc8cbe92998c1619f5bf2d18ca8acc0e3b6d3
aamirmd <aamirmd105@gmail.com>
2025-04-28 09:06:18 -0400

    Test all optimizers, no bugs found

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config:
-    NUM_CLIENTS = 10
-    NUM_ROUNDS = 50
+    NUM_CLIENTS = 2
+    NUM_ROUNDS = 1
     BATCH_SIZE = 32
     MODEL_NAME = "net"
-    DATASET = 'cifar10' # Literal['cifar10', 'cifar100', 'mnist']
-    PARTITIONER_TYPE = 'dirichlet' # Literal['uniform', 'dirichlet']
+    DATASET = 'cifar100' # Literal['cifar10', 'cifar100', 'mnist']
+    PARTITIONER_TYPE = 'uniform' # Literal['uniform', 'dirichlet']
     DIRILECT_ALPHA = 0.5 # parameter when using dirichlet (means how close to iid it is)
-    OPTIMIZER='r_adam'
+    OPTIMIZER='accsgd' # 'sgd', 'polyak', 'nesterov', 'rectified_adam', 'qhm', 'accsgd'
     LOCAL_EPOCHS=2
     LEARNING_RATE=0.6
     MOMENTUM=0.9
