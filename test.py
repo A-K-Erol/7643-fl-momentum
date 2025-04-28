7c547400940f660078f4bc886ef8fe89eca52d43
aamirmd <aamirmd105@gmail.com>
2025-04-28 08:35:28 -0400

    remove result directories

--- test.py
+++
@@ -1,9 +0,0 @@
-from optimizers import get_optimizer
-from models import Net
-
-
-net = Net()
-for opt in ['sgd', 'polyak', 'nesterov', 'r_adam', 'qhm', 'accsgd']:
-    print(f"Optimizer: {opt}")
-    optimizer = get_optimizer(net.parameters(), opt)
-    print(optimizer)  # This will print the optimizer object
